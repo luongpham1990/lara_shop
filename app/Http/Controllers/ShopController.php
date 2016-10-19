a053fe7f29da69a5f9a66d3173bc80a43dbbce48
Thân Văn Nghĩa <nghiait0111@gmail.com>
2016-10-19 08:50:29 +0000

    Revert "Merge branch 'developer' of https://gitlab.com/nghiatv/lara-shop"
    This reverts commit 481ebf0fc06268ee93160314ddf17b0aca2c0493

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -23,6 +23,7 @@ class ShopController extends Controller //chuyen de viet nhung cai hien thi ngoa
         $products = Product::paginate(6);
         $categories = Catalog::all();
         $relate_products = Product::paginate(4);
+
         $list_recommends = [];
 
         $calalogs = Catalog::get();
@@ -49,46 +50,10 @@ class ShopController extends Controller //chuyen de viet nhung cai hien thi ngoa
         $categories = Catalog::all();
         $product_detail = Product::find($id);
 
-        $calalogs = Catalog::get();
-        $list_recommends = [];
-
-        foreach ($calalogs as $catalog) {
-            $recommend_products = $catalog->products()->orderBy('view', 'desc')->take(3)->get();
-            array_push($list_recommends, $recommend_products);
-        }
-//        dd($product_detail->getAllImage());
-
         return view('shop.product-detail')->with([
             'products' => $products,
             'categories' => $categories,
             'product_detail' => $product_detail,
-            'recommend_products' => $list_recommends
-
-        ]);
-    }
-
-    public function showcatalog($id)
-    {
-        $products = Product::paginate(2);
-        $categories = Catalog::all();
-        $product = Product::all();
-
-        $all_catalogproducts = DB::table('products')->select('id', 'catalog_id', 'product_name', 'price', 'view', 'review', 'brand', 'status')->where('catalog_id', $id)->orderBy('view', 'DESC')->get();
-        foreach ($all_catalogproducts as $all_catalogproducts){
-        $img_id=$all_catalogproducts->id;
-        $imgs = DB::table('product_photos')
-            ->join('product_product_photos', 'product_photos.product_photo_id', '=', 'product_product_photos.product_photo_id')
-            ->join('products', 'product_product_photos.product_id', '=', 'products.id')
-            ->where('product_id',$img_id)
-            ->first();
-        }
-
-        dd($imgs->thumbnail_photo_link);
-        return view('shop.shop')->with([
-            'products' => $products,
-            'categories' => $categories,
-            'all_catalogproducts' => $all_catalogproducts,
-            'imgs' =>$imgs,
-        ]);
+            ]);
     }
 }
